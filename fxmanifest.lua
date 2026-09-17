d6105a1b9d2ddc4ca15027108ce79f26c878c7b8
ACE Studios <callum.rich2020@outlook.com>
2026-09-17 18:37:37 +0100

    Rename app from 'sd_carrier' to 'as_carrier'

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
 
-name 'sd_carrier'
+name 'as_carrier'
 author 'you'
 description 'Standalone Carrier/phone-bill app, registered into sd-phone through its real addCustomApp API - no sd-phone core files touched.'
 version '1.0.0'
